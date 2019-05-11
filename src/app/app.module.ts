92002fde0a3eeedf0b95f9974826f63385ef091c
Levi DeJean <levidejean@gmail.com>
2019-05-11 14:42:13 -0500

    getting unstuck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServerComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
